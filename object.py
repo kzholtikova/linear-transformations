994b2e7ce440fcb328764360882da6487c4b7ad7
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-05-28 21:22:19 +0300

    mirroring

--- object.py
+++ object.py
@@ -24,15 +24,24 @@ class Object:
         plt.title(self.title)
         plt.show()
 
-    def rotate(self, angle: int, dim: str):
+    def rotate(self, angle: int, axis: str):
         theta = np.radians(angle)
         rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
         if self.data.shape[1] == 3:
-            rotation_pivot = [1 if x == DIMENSIONS.index(dim) else 0 for x in range(self.data.shape[1])]
-            rotation_matrix = np.insert(rotation_matrix, DIMENSIONS.index(dim), rotation_pivot[:2], axis=0)
-            rotation_matrix = np.insert(rotation_matrix, DIMENSIONS.index(dim), rotation_pivot, axis=1)
+            rotation_pivot = [1 if x == DIMENSIONS.index(axis) else 0 for x in range(self.data.shape[1])]
+            rotation_matrix = np.insert(rotation_matrix, DIMENSIONS.index(axis), rotation_pivot[:2], axis=0)
+            rotation_matrix = np.insert(rotation_matrix, DIMENSIONS.index(axis), rotation_pivot, axis=1)
 
         self.data = np.dot(self.data, rotation_matrix)
 
     def scale(self, factor: int):
         self.data *= factor
+
+    def mirror(self, axis: str):
+        mirroring_matrix = np.eye(self.data.shape[1])
+        for i in range(self.data.shape[1]):
+            if i != DIMENSIONS.index(axis):
+                mirroring_matrix[i, i] = -1
+
+        self.data = np.dot(self.data, mirroring_matrix)
+        
