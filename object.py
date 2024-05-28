0881f237ab3a26cfd2eda120b230aa3bf49d3629
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-05-28 18:54:12 +0300

    3d-objects support

--- object.py
+++ object.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+DIMENSIONS = ['x', 'y', 'z']
 
 class Object:
     def __init__(self, data: np.ndarray, title: str, color: str):
@@ -9,9 +10,16 @@ class Object:
         self.color = color
 
     def plot(self):
-        plt.figure(figsize=(10, 5))
-        plt.plot(self.data[:, 0], self.data[:, 1], self.color, marker='o')
+        ax = plt.figure(figsize=(7, 5))
+        if self.data.shape[1] == 3:
+            ax = ax.add_subplot(projection='3d')
+            ax.set_zlabel('Z')
+        else:
+            ax = ax.add_subplot()
+
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.axis('equal')
+        ax.plot(*self.data.T, color=self.color, marker='o')
         plt.title(self.title)
-        plt.axis('equal')
         plt.show()
-    
